753853ac943b6c384561abfc88a0c89a8e7521c9
Tim Sehn <tim@dolthub.com>
2023-06-30 11:07:24 -0700

    Added Inserted data and Team Summary query

--- demo.py
+++ demo.py
@@ -5,14 +5,20 @@ from sqlalchemy import (
     Column,
     Integer,
     String,
-    ForeignKey
+    select,
+    insert,
+    delete,
+    ForeignKey,
+    MetaData
 )
+
 
 def main():
     engine = create_engine(
 	"mysql+mysqlconnector://root@127.0.0.1:3306/sql_alchemy_big_demo"
     )
 
+    # Build our tables
     setup_database(engine)
 
     # Raw SQL here to show what we've done
@@ -28,8 +34,18 @@ def main():
     # But after that nothing has changed so there is nothing to commit.
     dolt_commit(engine, "Tim <tim@dolthub.com>", "Created tables")
 
+    # Examine a Dolt system table: dolt_log
+    print_commit_log(engine)
+
+    insert_data(engine)
+
+    print_summary_table(engine)
+
+    dolt_commit(engine,
+                "Aaron <aaron@dolthub.com>",
+                "Inserted data into tables")
+
 def setup_database(engine):
-    from sqlalchemy import MetaData
     metadata_obj = MetaData()
 
     employees_table = Table(
@@ -63,21 +79,65 @@ def setup_database(engine):
     metadata_obj.create_all(engine)
 
 def insert_data(engine):
-    pass
+    metadata_obj = MetaData()
+    
+    employees = Table("employees", metadata_obj, autoload_with=engine)
+    teams = Table("teams", metadata_obj, autoload_with=engine)
+    employees_teams = Table("employees_teams",
+                            metadata_obj,
+                            autoload_with=engine)
+    
+    delete_employees_stmt = delete(employees)
+    delete_teams_stmt = delete(teams)
+    delete_employees_teams_stmt = delete(employees_teams)
+    with engine.connect() as conn:
+        conn.execute(delete_employees_teams_stmt)
+        conn.execute(delete_employees_stmt)
+        conn.execute(delete_teams_stmt)
+        conn.commit()
+    
+    stmt = insert(employees).values([
+        {'id':0, 'last_name':'Sehn', 'first_name':'Tim'}, 
+        {'id':1, 'last_name':'Hendriks', 'first_name':'Brian'}, 
+        {'id':2, 'last_name':'Son', 'first_name':'Aaron'}, 
+        {'id':3, 'last_name':'Fitzgerald', 'first_name':'Brian'}
+        ]);
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()
+
+    stmt = insert(teams).values([
+        {'id':0, 'name':'Engineering'},
+        {'id':1, 'name':'Sales'}
+    ])
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()
+
+    stmt = insert(employees_teams).values([
+        {'employee_id':0, 'team_id':0},
+        {'employee_id':1, 'team_id':0},
+        {'employee_id':2, 'team_id':0},
+        {'employee_id':0, 'team_id':1},
+        {'employee_id':3, 'team_id':1},
+    ])
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()
     
 def dolt_commit(engine, author, message):
+    # Dolt exposes version control writes as procedures
+    # Here, we use text to execute procedures.
+    #
+    # The other option is to do something like:
+    #
+    # conn = engine.raw_connection()
+    # results = conn.cursor().callproc('dolt_commit', arguments)
+    # conn.close()
+    #
+    # I like the text approach better.
+
     with engine.connect() as conn:
-        # Dolt exposes version control writes as procedures
-        # Here, we use text to execute procedures.
-        #
-        # The other option is to do something like:
-        #
-        # conn = engine.raw_connection()
-        # results = conn.cursor().callproc('dolt_commit', arguments)
-        # conn.close()
-        #
-        # I like the text approach better.
-        
         # -A means all tables
         conn.execute(
             text("CALL DOLT_ADD('-A')")
@@ -95,5 +155,56 @@ def dolt_commit(engine, author, message):
             commit = row[0]
         if ( commit ): 
             print("Created commit: " + commit )
+
+
+def print_commit_log(engine):
+    # Examine a dolt system table: dolt_log using reflection
+    metadata_obj = MetaData()
+    print("Commit Log:")
+
+    dolt_log = Table("dolt_log", metadata_obj, autoload_with=engine)
+    stmt = select(dolt_log.c.commit_hash,
+                  dolt_log.c.committer,
+                  dolt_log.c.message
+                  ).order_by(dolt_log.c.date.desc())
+
+    with engine.connect() as conn:
+        results = conn.execute(stmt)
+        for row in results:
+            commit_hash = row[0]
+            author      = row[1]
+            message     = row[2]
+            print("\t" + commit_hash + ": " + message + " by " + author)
+
+def print_summary_table(engine):
+    metadata_obj = MetaData()
     
+    employees = Table("employees", metadata_obj, autoload_with=engine)
+    teams = Table("teams", metadata_obj, autoload_with=engine)
+    employees_teams = Table("employees_teams",
+                            metadata_obj,
+                            autoload_with=engine)
+
+    print("Team Summary")
+    
+    stmt = select(employees.c.first_name,
+                  employees.c.last_name,
+                  teams.c.name
+                  ).select_from(
+                      employees
+                  ).join(
+                      employees_teams,
+                      employees.c.id == employees_teams.c.employee_id
+                  ).join(
+                      teams,
+                      teams.c.id == employees_teams.c.team_id
+                  ).order_by(teams.c.name.desc()); 
+    with engine.connect() as conn:
+        results = conn.execute(stmt)
+        for row in results:
+            first_name = row[0]
+            last_name  = row[1]
+            team_name  = row[2]
+            print("\t" + team_name + ": " + first_name + " " + last_name)
+            
 main()
